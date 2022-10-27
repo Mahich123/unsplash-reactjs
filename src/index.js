ba4fe661af144f68a95575221bf2f71d2491781c
Praveen <cjpraveen@hotmail.com>
2022-10-27 19:15:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import React, { useState } from "react"
+import unsplash from "./unsplash/unsplash.js"
 
-ReactDOM.render(
-  <React>
-    <App />
-  </React>,
-  document.getElementById('root')
-);
+function Home() {
+	const [images, setImages] = useState([])
+
+	unsplash("hello world",
+		(res) => {
+			setImages(res.images)
+		}
+	)
+
+	return (
+		images.map((image, i) => (
+			<a ref="nofollow" target="_blank" href={image}>{image}</a>
+		))
+	)
+}
+
+ReactDOM.render(<Home />, document.getElementById("root"))
