fb7885489b84b9b3abef1d9aa4e2bfcf836be42b
Praveen <cjpraveen@hotmail.com>
2022-10-27 21:48:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ function Home() {
 
 	return (
 		images.map((image, i) => (
-			<img src={image + "?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=60"} alt={i} />
+			<img src={image + "?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max"} alt={i} />
+	 		// regular: ?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max
+			// small: ?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max
+			// thumb: ?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max
+
 		))
 	)
 }
